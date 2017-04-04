2d0f5218ee544dfce6926ebbbdb1472489bb31ea
Hiroki Tanaka <tanaka.hiroki@gmail.com>
2017-04-04 18:23:23 +0900

    First

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using HoloToolkit.Unity.InputModule;
+using System;
+
+public class GameManager : MonoBehaviour, IInputClickHandler {
+
+    //public Camera myCamera;
+    public GameObject bulletPrefab;
+
+    public void OnInputClicked(InputClickedEventData eventData)
+    {
+        Debug.Log("fire");
+
+        //throw new NotImplementedException();
+
+        //Vector3 shootDir = myCamera.transform.forward.normalized;
+        Vector3 shootDir = transform.forward;
+
+        GameObject bullet = Instantiate(this.bulletPrefab);
+        Rigidbody rid = bullet.GetComponent<Rigidbody>();
+        rid.AddForce(shootDir * 100.0f);
+
+    }
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+
+}
